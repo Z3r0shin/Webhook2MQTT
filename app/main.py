817ba94d9351782a3f1b183880dd6a1407771d76
Zeroshin <z3r0shin@gmail.com>
2025-01-21 22:02:45 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -47,27 +47,27 @@ logger.info("set mqtt-path to '{}'".format(mqtt_path))
 
 app = Flask(__name__)
 
-def workit(params):
-    logger.info("workit:")
-    logger.info(params) 
-    params['timestamp'] = datetime.datetime.now().isoformat()
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
-    client.connect(mqtt_server, mqtt_port, 60)
-    client.publish(mqtt_path, json.dumps(params),qos=2,retain=True)
-    client.disconnect()
-
 #def workit(params):
 #    logger.info("workit:")
-#    logger.info(params)
+#    logger.info(params) 
 #    params['timestamp'] = datetime.datetime.now().isoformat()
+#    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+#    client.connect(mqtt_server, mqtt_port, 60)
+#    client.publish(mqtt_path, json.dumps(params),qos=2,retain=True)
+#    client.disconnect()
 
-#    try:
-#        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
-#        client.connect(mqtt_server, mqtt_port, 60)
-#        client.publish(mqtt_path, json.dumps(params), qos=0, retain=True)
-#        client.disconnect()
-#    except Exception as e:
-#        logger.error(f"Failed to publish to MQTT: {e}")
+def workit(params):
+    logger.info("workit:")
+    logger.info(params)
+    params['timestamp'] = datetime.datetime.now().isoformat()
+
+    try:
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+        client.connect(mqtt_server, mqtt_port, 60)
+        client.publish(mqtt_path, json.dumps(params), qos=0, retain=True)
+        client.disconnect()
+    except Exception as e:
+        logger.error(f"Failed to publish to MQTT: {e}")
 
 @app.route('/', methods=['POST'])
 def respond():
@@ -77,5 +77,5 @@ def respond():
     x.start()
     return Response(status=200)
 
-app.run (host = "0.0.0.0", port = 1357)
+#app.run (host = "0.0.0.0", port = 1357)
 
