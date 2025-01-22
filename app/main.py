c01fb3b5ea574773be424a1d9fe93b6c24519ec2
Zeroshin <z3r0shin@gmail.com>
2025-01-22 11:19:05 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 # -------------Output Logger
 
-mqtt_host = os.getenv("MQTT_HOST", "192.168.0.136")
+mqtt_host = os.getenv("MQTT_SERVER", "192.168.0.136")
 
 mqtt_port = os.getenv("MQTT_PORT", "1883")
 
@@ -31,7 +31,7 @@ mqtt_user = os.getenv("MQTT_USER")
 
 mqtt_pass = os.getenv("MQTT_PASS")
 
-logger.info(f"Configured mqtt host {mqtt_host} with port {mqtt_port} ")
+logger.info(f"Configured mqtt host {mqtt_server} with port {mqtt_port} ")
 
 app = Flask(__name__)
 
